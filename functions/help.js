bf9b11f8cbe68b279a052a922c1613e2188f8b48
Martius Lim <martius.lim.2015@sis.smu.edu.sg>
2018-08-16 20:24:07 +0800

    update help and start modules

--- functions/help.js
+++ functions/help.js
@@ -1,15 +1,13 @@
-const emoji = require('node-emoji');
-const fs = require('fs');
+function help(bot) {
+  bot.on('/help', (msg) => {
+    const res = `Thanks for asking! Here's what I can do for now:  
+- /register - save your details for priority access to our events!  
+- /feedback - send your feedback directly to my creators!`;
 
-function help(bot) {    
-    bot.on('/help', (msg) => {
-        let res = "/start - Greetings!\n /register - Register your profile with us";
-        
-        response = '';
-        bot.sendMessage(msg.from.id, '');
-    })
+    bot.sendMessage(msg.from.id, res);
+  });
 }
 
 module.exports = {
-    help: help
-}
+  help,
+};
--- functions/start.js
+++ functions/start.js
@@ -1,12 +1,14 @@
 const emoji = require('node-emoji');
+const config = require('../config');
 
-function start (bot) {
-    bot.on(['hi', '/start', '/hello', 'hello'], (msg) => {
-        console.log(msg)
-        bot.sendMessage(msg.from.id, 'Hello! I am **DotNetBot** %s, your friendly neighbourhood chatbot built for the **SMU Microsoft Student Community**.  \n\nSend /dotnet to learn more about what DotNet Society and our Microsoft Student Partners do or /help to see what else I can do!');
-    })
+function start(bot) {
+  bot.on(['/start', /[Hh](i|ello)/], (msg) => {
+    const res = `Hello ${msg.from.first_name}! I am *DotNetBot* ${emoji.get('robot_face')}, your friendly neighbourhood chatbot built for the *SMU Microsoft Student Community*.  We'd love for you to join us here:  \n${config.chat}  \n\nSend /help to see what else I can do!`;
+
+    bot.sendMessage(msg.from.id, res, { parseMode: 'Markdown' });
+  });
 }
 
 module.exports = {
-    start: start
-}
+  start,
+};
