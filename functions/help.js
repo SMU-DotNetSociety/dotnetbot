9a165f96bcee6fbb26ffe33b209c1f6608360f2c
Martius Lim <martius.lim.2015@sis.smu.edu.sg>
2018-08-16 23:04:20 +0800

    remove /feedback from help function

--- functions/help.js
+++ functions/help.js
@@ -1,8 +1,8 @@
 function help(bot) {
   bot.on('/help', (msg) => {
     const response = `Thanks for asking! Here's what I can do for now:  
-- /register - save your details for priority access to our events!  
-- /feedback - send your feedback directly to my creators!`;
+- /register - save your details for priority access to our events!`;
+    // - /feedback - send your feedback directly to my creators!
 
     bot.sendMessage(msg.from.id, response);
   });
